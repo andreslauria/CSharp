354180e26aa27f20037717f60200354963f1b8d3
Andres francisco Lauria <andreslauria@gmail.com>
2022-06-26 21:38:28 -0300

    Ejercicios
    Ejercicios 1 y 2

---
+++ Ejercicio1-2/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribir su nombre: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nHola " + name);
+
+            DateTime horaDePc = DateTime.Now;
+            Console.Write("\nLa hora actual es: ");
+            Console.WriteLine(horaDePc.Hour + ":" + horaDePc.Minute + ":" + horaDePc.Second);
+        }
+    }
+}
